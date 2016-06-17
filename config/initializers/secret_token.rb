98c80622d76e778ed597fadf99eaf5d0027fe9de
khan.WH.Lee <r02945033@ntu.edu.tw>
2016-06-17 09:26:57 +0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snasa::Application.config.secret_key_base = '42c22a93ca091d6e45d4b5150f7cf993467583744616b6befb1b59d8e912458f0799d098f0cb06236ba5b210fe708a900adf34fc588f8a7dbc25c63d66e25e4f'
