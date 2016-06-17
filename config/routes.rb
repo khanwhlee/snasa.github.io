39ec1f646f97ca0593461c29840b529258948bc3
khan.WH.Lee <r02945033@ntu.edu.tw>
2016-06-17 13:15:36 +0800

    finish new session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Snasa::Application.routes.draw do
-  devise_for :users
+  
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+
   root to: "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
