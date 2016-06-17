bcc6b0aac851fc440702e047e3bc34532dd14b65
khan.WH.Lee <r02945033@ntu.edu.tw>
2016-06-17 18:13:15 +0800

    signinstyle part 1 finish

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160617051843) do
+ActiveRecord::Schema.define(version: 20160617070122) do
 
   create_table "profiles", force: true do |t|
     t.datetime "created_at"
@@ -31,9 +31,11 @@ ActiveRecord::Schema.define(version: 20160617051843) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
