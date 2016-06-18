db4e9ad5a85b79b5c5837c2c2339ce1f952d1bf1
khan.WH.Lee <r02945033@ntu.edu.tw>
2016-06-18 12:36:23 +0800

    signin keyboard done

--- app/assets/javascripts/sign_in.js
+++ app/assets/javascripts/sign_in.js
@@ -1,17 +1,44 @@
 
 $(document).ready(function(){
-    $('.dot').hide();
+    $('.dot , .field, #login').hide();
+    $('.key').fadeTo('fast', 0.5);
 
-});
+    $('.key').mouseenter(function() {
+        $(this).fadeTo('fast', 1);
+    });
+    $('.key').mouseleave(function() {
+        $(this).fadeTo('fast', 0.5);
+    });
 
-$(document).ready(function(){
     $('.key').click(function() {
-    	if ($('#first').is(":visible") && $('#second').is(":hidden")){
-    		$('#second').fadeIn('fast');
-    	}else if($('#first').is(":visible") && $('#second').is(":visible")){
-    		$('#third').fadeIn('fast');
-    	}else{
-    		$('#first').fadeIn('fast');
-    	}	
+        if ($('#first').is(":visible") && $('#second').is(":hidden")){
+            $('#second').fadeIn('fast');
+        }else if($('#first').is(":visible") && $('#second').is(":visible")){
+            $('#third').fadeIn('fast');
+        }else{
+            $('#first').fadeIn('fast');
+        }
     });
 });
+
+function input(e) {
+    var idInput = document.getElementById("idInput");
+    var pwInput = document.getElementById("pwInput");
+    if(idInput.value.length < 2){     //要記得加value tbInput是取出表單
+        idInput.value = idInput.value + e.value;
+    }
+    else if(idInput.value.length ==2){
+        idInput.value = idInput.value + e.value;
+        if(idInput.value < 100){
+            $('.field , #login').fadeIn('slow');
+        }
+    }
+    else if(idInput.value.length > 2){
+        pwInput.value = pwInput.value + e.value;
+    }
+}
+
+
+
+
+
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,9 +1,14 @@
 class Users::SessionsController < Devise::SessionsController
-  before_filter :configure_sign_in_params, only: [:create]
+  prepend_before_action :require_no_authentication, only: [:new, :create]
+  prepend_before_action :allow_params_authentication!, only: :create
+  prepend_before_action :verify_signed_out_user, only: :destroy
 
   # GET /resource/sign_in
     def new
-      super
+      self.resource = resource_class.new(sign_in_params)
+      clean_up_passwords(resource)
+      yield resource if block_given?
+      respond_with(resource, serialize_options(resource))
     end
 
   # POST /resource/sign_in
@@ -17,7 +22,9 @@ class Users::SessionsController < Devise::SessionsController
 
   # DELETE /resource/sign_out
     def destroy
-      super
+      signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+      yield if block_given?
+      respond_to_on_destroy
     end
 
     protected
@@ -25,5 +32,29 @@ class Users::SessionsController < Devise::SessionsController
   # You can put the params you want to permit in the empty array.
     def configure_sign_in_params
       devise_parameter_sanitizer.for(:sign_in) << :attribute
+    end
+
+    private
+
+    def verify_signed_out_user
+    if all_signed_out?
+
+      respond_to_on_destroy
+    end
+    end
+
+    def all_signed_out?
+      users = Devise.mappings.keys.map { |s| warden.user(scope: s, run_callbacks: false) }
+
+      users.all?(&:blank?)
+    end
+
+    def respond_to_on_destroy
+      # We actually need to hardcode this as Rails default responder doesn't
+      # support returning empty response on GET request
+      respond_to do |format|
+        format.all { head :no_content }
+        format.any(*navigational_formats) { redirect_to after_sign_out_path_for(resource_name) }
+      end
     end
 end
